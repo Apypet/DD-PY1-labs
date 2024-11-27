dda46e6e67639e431b1c25e3458f96cba81ab4b3
Apypet <nastya.yagin@gmail.com>
2024-11-27 22:12:52 +0300

    Update #2.py

--- 2 lab/#2.py
+++ 2 lab/#2.py
@@ -2,16 +2,12 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
-
+money_capital = 0  # Общая сумма, необходимая для покрытия расходов
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
-total_needed = 0  # Общая сумма, необходимая для покрытия расходов
-current_spend = spend  # Текущие расходы на месяц
 for month in range(months):
-        if month > 0:  # На втором месяце и далее расходы увеличиваются
-            current_spend *= (1 + increase)
-
-        # Вычисляем нехватку средств после зарплаты
-        deficit = current_spend - salary
+        if month > 0:  #На втором месяце и далее расходы увеличиваются
+            spend *= (1 + increase)
+        deficit = spend - salary #Вычисляем нехватку средств после зарплаты
         if deficit > 0:
-            total_needed += deficit
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(total_needed))
+            money_capital += deficit
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
